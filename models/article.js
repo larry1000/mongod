c8d65f846e300a93d22e15adeac6bafd88ac7ef8
Larry L <larrylight2003@msn.com>
2018-12-08 23:09:17 -0500

    first one

---
+++ models/article.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+  title: String
+});
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
