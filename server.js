052cac1b31322b299613ef954b8139447db80347
Larry L <larrylight2003@msn.com>
2018-12-11 21:56:00 -0500

    help

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/week18Populater";
 
 // Connect to the Mongo DB
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Routes
